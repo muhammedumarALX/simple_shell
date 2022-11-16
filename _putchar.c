82d2a2779ae14fed2737f16343ddd9ca8e747f59
Cyber-Joe <elegbedeayomide07@gmail.com>
2022-11-15 17:42:44 -0800

    touchup

--- _putchar.c
+++ _putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * _putchar - write the character c to stdout
- * @c; The character to print
+ * @c: The character to print
  *
  * Return: on success 1
  * on error, -1 is returned, and errno is set appropriately
